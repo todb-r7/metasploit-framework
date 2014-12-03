a9ecd22a135e040d8b5c7ce2f913cf14f4b75ab1
Tod Beardsley <tod_beardsley@rapid7.com>
2014-12-03 14:14:32 -0600

    Okay, relative links!

--- metakitty/config.rb
+++ metakitty/config.rb
@@ -3,6 +3,9 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+# Since this is a project page, all links should be relative.
+set :relative_links, true
+
 activate :deploy do |deploy|
   deploy.method = :git
   deploy.remote = 'upstream'
