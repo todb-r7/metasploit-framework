0c75eec8e88f26e1c55aa3e6a3006c1851304964
Tod Beardsley <tod_beardsley@rapid7.com>
2015-03-17 12:11:44 -0500

    Update the tool
    Longer description, if needed, wrapped at 72 characters.
    
    [See #XXXX]

--- tools/dev/import-keybase.sh
+++ tools/dev/import-keybase.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # COMMITER_KEYS_URL='https://raw.githubusercontent.com/wiki/rapid7/metasploit-framework/Committer-Keys.md'
-COMMITTER_KEYS_URL='https://gist.githubusercontent.com/todb-r7/38869e2dad97b64cc00a/raw/59aeb1bfec834dd6da412eb6dce8b8afba450ae1/k.md'
+COMMITTER_KEYS_URL='https://gist.githubusercontent.com/todb-r7/38869e2dad97b64cc00a/raw/9064e015fc5326c79369823bb9860c8a95c1d6d8/k.md'
 KEY_URLS=$(
  \curl -sSL $COMMITTER_KEYS_URL |
- awk '$4 ~/https:\/\/keybase.io\//' |
- sed 's#.*\(https://keybase.io/[^)]*\).*#\1/key.asc#'
+ \awk '$4 ~/https:\/\/keybase.io\//' |
+ \sed 's#.*\(https://keybase.io/[^)]*\).*#\1/key.asc#'
 )
 for key in $KEY_URLS; do
   echo Importing $key...
